b6970624917b410bfe4b7ad9c6c39610a3dc4de9
Taras <baran.taras.dev@gmail.com>
2023-07-13 15:40:33 +0200

    feature/createHomePage - created home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,16 @@
+import { useTranslation } from "react-i18next";
+import './Home.scss';
+
 const Home = () => {
+  const {t} = useTranslation();
+
   return (
     <section>
-      Home
+      <div className="container">
+        <h1 className="home__title">
+          {t('welcome')}
+        </h1>
+      </div>
     </section>
   );
 };
