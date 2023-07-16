d7b8e3611ed80356708cd7f8006a73d9429ea33d
Taras <baran.taras.dev@gmail.com>
2023-07-17 00:29:54 +0200

    updated i18n file

--- src/common/i18n.ts
+++ src/common/i18n.ts
@@ -1,16 +1,18 @@
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
-import detector from "i18next-browser-languagedetector";
 import {initReactI18next} from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n.use(Backend)
-    .use(detector)
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        ns: ['translation'],
+        defaultNS: 'translation',
         fallbackLng: 'en',
         debug: false,
-        interpolation: {
-            escapeValue: false,
+        backend: {
+            loadPath: './locales/{{lng}}/{{ns}}.json',
         },
     });
 
